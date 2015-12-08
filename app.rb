df00cad894c36ab8a1c1aa13eb77da2bee2193fb
Adrian Maseda <amaseda@gmail.com>
2015-12-08 10:13:05 -0500

    add active record.

--- app.rb
+++ app.rb
@@ -1,5 +1,13 @@
-require 'httparty'
-require 'sinatra'
-require 'sinatra/contrib'
-require_relative 'models/spotify.rb'
+require "httparty"
+require "sinatra"
+require "sinatra/contrib"
+require "active_record"
+require_relative "models/spotify.rb"
 require_relative "env.rb"
+
+get "/" do
+  erb :index
+end
+
+get "/callback" do
+end
